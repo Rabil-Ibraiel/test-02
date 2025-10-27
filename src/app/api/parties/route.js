b206b1355c202c47b7b861248e154eb92d8046d2
unknown <rabilabrail@gmail.com>
2025-10-27 03:36:51 +0300

    commit_03

--- src/app/api/parties/route.js
+++ src/app/api/parties/route.js
@@ -1,4 +1,4 @@
-import prisma from "../../lib/prisma";
+import prisma from "../../../lib/prisma";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
@@ -46,3 +46,4 @@ export default async function handler(req, res) {
 
   res.status(405).end();
 }
+
